b79afdfd12cdcb3737576a1bd46fa49f44856274
Joe <joe.chien23@gmail.com>
2016-10-14 11:31:08 +0800

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-/home/hadoop/spark/bin/spark-submit --class ALS1predict --master local --executor-memory 2G ALS1predict.jar  s3a://jccsvdata/test3.data s3a://jcrecommand/json3
+/home/hadoop/spark/bin/spark-submit --class ALSpredict --master local --executor-memory 2G ALSpredict.jar  s3a://jccsvdata/test3.data s3a://jcrecommand/result
